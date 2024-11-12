938ba7d6dd298ad4fb3765cece56db6d93562d01
giuseppecasaburi <enzobiondi0000@libero.it>
2024-11-12 16:42:39 +0100

    feat: completamento primi due esercizi

---
+++ js/script.js
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SNACK 1
+const arrayObj = [{nome: "BMW", peso: "3"}, {nome: "Audi", peso: "2"}, {nome: "Alfa romeo", peso: "5"}, {nome: "Range Rover", peso: "2"}, {nome: "Tesla", peso: "1.5"}];
+
+// CICLO CHE CONFRONTA I PESI DELLE BICI E NE STABILISCE LA PIù LEGGERA
+let lessBike = arrayObj[0].peso;
+for (let i = 0; i < arrayObj.length; i++) {
+    let currItem = arrayObj[i].peso;
+    if (currItem < lessBike) {
+        lessBike = currItem;
+    }
+}
+
+console.log(lessBike);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// SNACK 2
+const arraySquad = [{nome: "Barcellona", puntiFatti: 0, falliSubiti: 0}, {nome: "Real madrid", puntiFatti: 0, falliSubiti: 0}, {nome: "Inter", puntiFatti: 0, falliSubiti: 0}, {nome: "Manchester united", puntiFatti: 0, falliSubiti: 0}, {nome: "Manchester city", puntiFatti: 0, falliSubiti: 0}];
+
+// GENERATORE NUMERICO
+function generator (min, max) {
+   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// CICLO FOR PER AGGIUNGERE IL NUMERO DEI FALLI SUBITI E DEI PUNTI FATTI AL NOSTRO ARRAY
+for (let i = 0; i < arraySquad.length; i++) {    
+    arraySquad[i].puntiFatti += (generator(10, 20));
+    arraySquad[i].falliSubiti += (generator(10, 20));
+}
+console.log(arraySquad);
+
+// CICLO CHE CREA UN NUOVO ARRAY E INSERISCE AL SUO INTERNO SOLAMENTE IL NOME DELLA SQUADRA E I FALLI SUBITI
+const arraySquadNew = [];
+for (let i = 0; i < arraySquad.length; i++) {
+    arraySquadNew.push(arraySquad[i].nome);
+    arraySquadNew.push(arraySquad[i].falliSubiti);
+}
+console.log(arraySquadNew);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// SNACK 3
